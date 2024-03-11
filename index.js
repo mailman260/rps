b51b0c09812660edcbfd6e9f05a8725e000bc9eb
Ryan Harris <109178023+mailman260@users.noreply.github.com>
2024-03-10 22:37:19 -0400

    Added function to finish off

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("hey")
-
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
@@ -7,4 +5,81 @@ function getComputerChoice(){
     return pick;
 }
 
-getComputerChoice();
+
+function getWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "player";
+    }
+    else {
+        return "computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = getWinner(playerSelection, computerSelection);
+        if(result == "tie"){
+            return "It's a tie!"
+        }
+        else if(result == "player"){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else if(result == "computer"){
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+}
+
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock, Paper, or Scissors?")
+        if (choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+
+
+function playGame(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome to my game")
+    for (i =0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(" ")
+        if(getWinner(playerSelection, computerSelection) == "player"){
+            scorePlayer++;
+        }
+        else if(getWinner(playerSelection, computerSelection) == "computer"){
+            scoreComputer++;
+        }
+    }
+    console.log("Game Over")
+    if(scorePlayer > scoreComputer){
+        console.log("The player is the victor");
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log("The computer is reigning champion now");
+    }else {
+        console.log("");
+    }
+}
+ 
+playGame()
+
+
+
